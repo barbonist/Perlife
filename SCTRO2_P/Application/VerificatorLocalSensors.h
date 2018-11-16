9a18a232f3ee55e119b0b5b40eea26e81516523e
Vincenzo Perrone <vincenzo.perrone@lean.it>
2018-11-16 16:44:26 +0100

    Aggiunti file header protective nom versionati

---
+++ SCTRO2_P/Application/VerificatorLocalSensors.h
@@ -0,0 +1,26 @@
+/*
+ * VerificatorLocalSensors.h
+ *
+ *  Created on: 02/set/2018
+ *      Author: W5
+ */
+
+#ifndef APPLICATION_VERIFICATORLOCALSENSORS_H_
+#define APPLICATION_VERIFICATORLOCALSENSORS_H_
+
+void InitVerificatorLocalParams(void);
+
+void VerifyArterialPressure(uint16_t ValPress);
+void VerifyOxygenPressure(uint16_t Value);
+void VerifyTubPressure(uint16_t Value);
+void VerifyFilterPressure(uint16_t  Value);
+void VerifyVenousPressure(uint16_t  Value);
+void VerifyArterialPressure(uint16_t  Value);
+
+
+void VerifyPlateTemp(float Value);
+void VerifyArtTemp(float Value);
+void VerifyVenTemp(float Value);
+void VerifyFluidTemp(float Value);
+
+#endif /* APPLICATION_VERIFICATORLOCALSENSORS_H_ */
---
+++ SCTRO2_P/Application/VerificatorRxCanValues.h
@@ -0,0 +1,36 @@
+/*
+ * VerificatorRxCanValues.h
+ *
+ *  Created on: 02/mag/2018
+ *      Author: W5
+ */
+
+#ifndef APPLICATION_VERIFICATORRXCANVALUES_H_
+#define APPLICATION_VERIFICATORRXCANVALUES_H_
+
+typedef void(*TAlarmAction)(void);// TAlarmAction;
+
+typedef struct {
+	uint8_t AlarmCounter;
+	uint8_t	CountTreshold;
+	bool AlarmConditionPending;
+	bool AlarmActive;
+	TAlarmAction AlarmAction;
+	uint8_t SecondaryActionTimer;
+	uint8_t SecondaryActionTimerTreshold; // if 0 , never execute 2nd action
+	TAlarmAction SecondaryAlarmAction;
+} TAlarmTimer;
+
+
+
+void InitVerificatorRx(void);
+void VerifyRxState(uint16_t State, uint16_t Parent, uint16_t Child, uint16_t Guard);
+void VerifyRxPressures(uint16_t PressFilter, uint16_t PressArt, uint16_t PressVen, uint16_t PressLevelx100, uint16_t PressOxy);
+void VerifyRxTemperatures(uint16_t TempArtx10, uint16_t TempFluidx10, uint16_t TempVenx10, uint16_t TempPlatex10);
+void VerifyRxAirAlarm( uint8_t RxAirAlarm );
+void VerifyRxPinchPos( uint8_t Pinch0Pos , uint8_t Pinch1Pos ,  uint8_t Pinch2Pos);
+void VerifyRxPumpsRpm(uint16_t SpeedPump0Rpmx100, uint16_t SpeedPump1Rpmx100, uint16_t SpeedPump2Rpmx100, uint16_t SpeedPump3Rpmx100);
+void ManageRxAlarmCode(uint16_t AlarmCode);
+void NotifyCanOnline(bool Online);
+
+#endif /* APPLICATION_VERIFICATORRXCANVALUES_H_ */
